217b5df349617d9c1f7a2be2a1d6b1bcaab69945
Oscar Hdz <oscardavid.hernandez.mx@gmail.com>
2020-03-20 15:34:24 -0600

    Unit Test - Validate Enabled Endpoints

--- packages/web-server/test/Salary.spec.js
+++ packages/web-server/test/Salary.spec.js
@@ -1,4 +1,23 @@
+const router = require('../endpoints/Salary');
 
 describe('Salary', () => {
+
+  describe('Router', () => {
+    const routesToValidate = [
+      {
+        method: 'post',
+        url: '/salary'
+      }
+    ];
+
+    routesToValidate.forEach(routeToValidate => {
+      it(`should validate that '${routeToValidate.method}' '${routeToValidate.url}' route is enabled`, () => {
+        expect(router.stack.find(layer => layer.route.path === routeToValidate.url && layer.route.methods[routeToValidate.method] === true));
+      });
+    })
+
+
+
+  });
 
 });
--- packages/web-server/test/TeamGoals.spec.js
+++ packages/web-server/test/TeamGoals.spec.js
@@ -1,4 +1,43 @@
+const router = require('../endpoints/TeamGoals');
 
 describe('TeamGoals', () => {
+
+  describe('Router', () => {
+    const routesToValidate = [
+      {
+        method: 'get',
+        url: '/team-goals'
+      },
+      {
+        method: 'get',
+        url: '/team-goals/:team'
+      },
+      {
+        method: 'post',
+        url: '/team-goals'
+      },
+      {
+        method: 'put',
+        url: '/team-goals/:team'
+      },
+      {
+        method: 'delete',
+        url: '/team-goals/:team'
+      },
+      {
+        method: 'post',
+        url: '/team-goals/batch'
+      }
+    ];
+
+    routesToValidate.forEach(routeToValidate => {
+      it(`should validate that '${routeToValidate.method}' '${routeToValidate.url}' route is enabled`, () => {
+        expect(router.stack.find(layer => layer.route.path === routeToValidate.url && layer.route.methods[routeToValidate.method] === true));
+      });
+    })
+
+
+
+  });
 
 });
